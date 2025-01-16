3ee58af19d847badc68ad33b4de87015fdbaa49d
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2025-01-16 10:20:28 +0100

    nom

--- __init__.py
+++ __init__.py
@@ -49,7 +49,7 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/fiche_client/<text:nom>')
+@app.route('/fiche_nom/<text:nom>')
 def Readfiche(nom):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
