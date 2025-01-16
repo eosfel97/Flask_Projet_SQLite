d7d98ca17d37902ae7e5e51e01ff6ee65cecdb02
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2025-01-16 12:33:15 +0100

    teste ficheclient

--- __init__.py
+++ __init__.py
@@ -90,6 +90,59 @@ def ReadNom(nom):
     conn.close()
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
-                                                                                                                                       
+
+
+
+@app.route('/fiche_client_bibi/<int:post_id>')
+def Readfiche_client(post_id):
+    conn = sqlite3.connect('bibliotheque.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+    data = cursor.fetchall()
+    conn.close()
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data_bibi.html', data=data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
   app.run(debug=True)
