4fb4afb9440163e15169f9462a8bbfddd4335f5d
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2025-01-16 10:27:59 +0100

    jji

--- __init__.py
+++ __init__.py
@@ -53,7 +53,7 @@ def Readfiche(post_id):
 def Readfiche(nom):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom,))
+    cursor.execute('SELECT * FROM clients WHERE nom LIKE ?', ('%' + nom + '%',)) 
     data = cursor.fetchall()
     conn.close()
     # Rendre le template HTML et transmettre les données
